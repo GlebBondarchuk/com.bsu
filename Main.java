07dbee105f7fc9fe5112584ddea85ec0549ddec6
Gleb <gleb.exadel@mail.ru>
2014-03-02 00:53:20 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package com.bsu;
-
-public class Main {
-    public static synchronized void main(String[] args) {
-        for(int i = 0; i < 10;) {
-            throw new RuntimeException();
-        }
-    }
-}
